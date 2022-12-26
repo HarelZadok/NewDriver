e747f8dede653f188a658685b6d4bba6220351d1
Harel Zadok <42193605+IShouldBeGood@users.noreply.github.com>
2022-12-26 02:50:22 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ChoosePage from './components/ChoosePage';
+import RegisterPage from './components/RegisterPage';
+import {StatusBar} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+export default function App()
+{
+  return <NavigationContainer>
+    <Stack.Navigator initialRouteName={'ChoosePage'} screenOptions={{headerShown: false}}>
+      <Stack.Screen name={'ChoosePage'} component={ChoosePage}/>
+      <Stack.Screen name={'RegisterPage'} component={RegisterPage}/>
+    </Stack.Navigator>
+    <StatusBar translucent backgroundColor={'transparent'}/>
+  </NavigationContainer>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/ChoosePage.js
@@ -0,0 +1,78 @@
+import {View, Text, TouchableOpacity, Image, StyleSheet} from "react-native";
+
+export default function ChoosePage({navigation}) {
+    return <View style={styles.container}>
+        <View style={styles.header}>
+            <View style={styles.newDriverView}>
+                <Text style={{fontWeight: 'bold', fontSize: 18}}>נהג חדש</Text>
+            </View>
+        </View>
+        <Image style={styles.carImage} source={require('../resources/Car.png')}/>
+        <View style={styles.body}>
+            <TouchableOpacity style={[styles.button, {marginTop: 50}]} onPress={() => moveToRegisterPage(navigation)}>
+                <Text style={{fontWeight: 'bold', fontSize: 20, color: '#183658'}}>המשך בתור נהג חדש</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => moveToRegisterPage(navigation)}>
+                <Text style={{fontWeight: 'bold', fontSize: 20, color: '#183658'}}>המשך בתור מלווה</Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+}
+
+const moveToRegisterPage = (navigation) => {
+    navigation.navigate('RegisterPage');
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    header: {
+        backgroundColor: '#183658',
+        height: 280,
+        borderBottomRightRadius: 30,
+        borderBottomLeftRadius: 30,
+        alignItems: 'flex-end',
+    },
+    body: {
+        flex: 1,
+        height: 'auto',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    newDriverView: {
+        backgroundColor: '#e9ba00',
+        height: 60,
+        width: 150,
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: '#000',
+        borderWidth: 3,
+        marginRight: 20,
+        marginTop: 40,
+    },
+    carImage: {
+        height: 100,
+        width: 200,
+        position: 'absolute',
+        top: 205,
+        bottom: 0,
+        left: 10,
+        right: 0,
+        transform: [{scaleX: -1}]
+    },
+    button: {
+        backgroundColor: '#eee',
+        padding: 20,
+        marginTop: 20,
+        width: "90%",
+        height: 70,
+        borderWidth: 1,
+        borderColor: '#183658',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
